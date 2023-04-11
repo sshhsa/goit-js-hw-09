4eaff690e68d9807835ad5027bf83a09613d9aab
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-04-11 18:02:22 +0300

    First task is completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const throttle = require('lodash.throttle');
 
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+let asyncFn = null;
+
+buttonStart.addEventListener('click', throttle(onButtonClickStart, 2000));
+buttonStop.addEventListener('click', onButtonClickStop);
+
+function onButtonClickStart(event) {
+  asyncFn = setInterval(() => {
+    function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+    }
+
+    const newColor = getRandomHexColor();
+    document.body.style.backgroundColor = newColor;
+  }, 1000);
+  buttonStop.removeAttribute('disabled');
+  if (!buttonStop.disabled) {
+    buttonStart.setAttribute('disabled', true);
+  }
+}
+
+function onButtonClickStop() {
+  clearInterval(asyncFn);
+  buttonStart.removeAttribute('disabled');
+  if (!buttonStart.disabled) {
+    buttonStop.setAttribute('disabled', true);
+  }
+}

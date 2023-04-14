fe2a83150648511bf6215480dd5af7dd6328b203
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-04-14 09:30:29 +0300

    Task 2 is in the process

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,17 +1,19 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const currentDate = new Date();
+
 const datetimePicker = flatpickr('#datetime-picker', {
   enableTime: true,
   dateFormat: 'Y-m-d H:i',
   onClose(selectedDates) {
     const selectedDate = selectedDates[0];
-    const currentDate = new Date();
 
     if (selectedDate <= currentDate) {
       window.alert('Please choose a date in the future');
       button.setAttribute('disabled', true);
-    } else {
+    }
+    if (selectedDate >= currentDate) {
       button.removeAttribute('disabled', true);
     }
   },
@@ -19,3 +21,34 @@ const datetimePicker = flatpickr('#datetime-picker', {
 
 const input = document.getElementById('datetime-picker');
 const button = document.querySelector('[data-start]');
+
+const daysItem = document.querySelector('[data-days]');
+const hoursItem = document.querySelector('[data-hours]');
+const minutesItem = document.querySelector('[data-minutes]');
+const secondsItem = document.querySelector('[data-seconds]');
+
+function updateTimer() {
+  if (datetimePicker.selectedDates.length > 0) {
+    const update = datetimePicker.selectedDateElem[0] - currentDate;
+
+    if (update <= 0) {
+      clearInterval(intervalId);
+      return;
+    }
+
+    const days = Math.floor(update / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((update / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((update / (1000 * 60)) % 60);
+    const seconds = Math.floor((update / 1000) % 60);
+
+    daysItem.textContent = days.toString().padStart(2, '0');
+    hoursItem.textContent = hours.toString().padStart(2, '0');
+    minutesItem.textContent = minutes.toString().padStart(2, '0');
+    secondsItem.textContent = seconds.toString().padStart(2, '0');
+  }
+}
+
+let intervalId;
+button.addEventListener('click', () => {
+  intervalId = setInterval(updateTimer, 1000);
+});
